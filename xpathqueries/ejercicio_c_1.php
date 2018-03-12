c03a74a487d2c89175ca6b9b870ec89acad000c0
JTorrus <jtorrusgalindo@gmail.com>
2018-03-12 07:30:04 +0100

    Exercise 6 finished

--- xpathqueries/ejercicio_c_1.php
+++ xpathqueries/ejercicio_c_1.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Javier_T
- * Date: 3/12/2018
- * Time: 6:49 AM
- */
+
+$doc = new DOMDocument();
+$doc->load("../xml/rss.xml");
+
+$xpath = new DOMXPath($doc);
+
+$result = $xpath->query("//item/title");
+
+foreach ($result as $node) {
+    echo $node->textContent."<br>";
+}
