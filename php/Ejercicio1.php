c33d0b48ab1a619a60ecda0cc27d3fda8dae6540
JTorrus <jtorrusgalindo@gmail.com>
2018-03-12 06:52:36 +0100

    Structure created for each exercise

--- php/Ejercicio1.php
+++ php/Ejercicio1.php
@@ -1,1 +1,39 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <a href="../xpathqueries/ejercicio_a_1.php">(menu.xml) Nombre de todos los platos usando rutas absolutas</a>
+        </li>
+        <li>
+            <a href="../xpathqueries/ejercicio_a_2.php">(menu.xml) Descripcion del 4to plato</a>
+        </li>
+        <li>
+            <a href="../xpathqueries/ejercicio_a_3.php">(menu.xml) Suma de las categorias de todos los platos</a>
+        </li>
+        <li>
+            <a href="../xpathqueries/ejercicio_b_1.php">(bookstore.xml) Libro con id="bk104"</a>
+        </li>
+        <li>
+            <a href="../xpathqueries/ejercicio_b_2.php">(bookstore.xml) Titulo y descripcion de todos los libros usando rutas relativas</a>
+        </li>
+        <li>
+            <a href="../xpathqueries/ejercicio_c_1.php">(rss.xml) Titulo de las noticias, pero no el titulo principal</a>
+        </li>
+        <li>
+            <a href="../xpathqueries/ejercicio_c_2.php">(rss.xml) Contenido de la penultima noticia</a>
+        </li>
+        <li>
+            <a href="../xpathqueries/ejercicio_c_3.php">(rss.xml) Numero de noticias que hay</a>
+        </li>
+    </ul>
+
+</body>
+</html>
---
+++ xpathqueries/ejercicio_c_2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Javier_T
+ * Date: 3/12/2018
+ * Time: 6:50 AM
+ */
